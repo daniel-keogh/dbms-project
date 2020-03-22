86b223135bf0ad8e3e6d1c2e3ece15265f4f1177
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-22 17:37:08 +0000

    Add the other suggested tables

--- mulcahy_dental.sql
+++ mulcahy_dental.sql
@@ -1,26 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 22, 2020 at 04:38 PM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `mulcahy_dental`
---
+DROP DATABASE IF EXISTS `mulcahy_dental`;
+CREATE DATABASE `mulcahy_dental`;
+USE `mulcahy_dental`;
 
 -- --------------------------------------------------------
 
@@ -33,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `appointments` (
   `id` int(10) UNSIGNED NOT NULL,
   `date` datetime NOT NULL,
   `booking` enum('online','post','phone','drop-in') NOT NULL,
-  `cancelled` tinyint(1) NOT NULL DEFAULT '0',
+  `is_cancelled` tinyint(1) NOT NULL DEFAULT '0',
   `follow_up` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -50,7 +30,8 @@ CREATE TABLE IF NOT EXISTS `bill` (
   `date` date NOT NULL,
   `amount` float NOT NULL,
   `patient_id` int(10) NOT NULL,
-  `appointment_id` int(10) NOT NULL
+  `appointment_id` int(10) NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -83,8 +64,33 @@ CREATE TABLE IF NOT EXISTS `payment` (
   `patient_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialist`
+--
+
+DROP TABLE IF EXISTS `specialist`;
+CREATE TABLE IF NOT EXISTS `specialist` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone_no` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `treatment`
+--
+
+DROP TABLE IF EXISTS `treatment`;
+CREATE TABLE IF NOT EXISTS `treatment` (
+  `id` int(10) NOT NULL,
+  `patient_id` int(10) NOT NULL,
+  `appointment_id` int(11) NOT NULL,
+  `image` blob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='treatment';
