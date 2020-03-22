1351653b3cc05ed1e93945246d797e9be6dfa9d4
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-22 21:34:54 +0000

    Add some dummy data

--- mulcahy_dental.sql
+++ mulcahy_dental.sql
@@ -13,12 +13,25 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `dob` date DEFAULT NULL,
-  `phone_no` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `amount_owed` float NOT NULL DEFAULT 0,
+  `phone_no` varchar(20) NOT NULL UNIQUE,
+  `email` varchar(50) NOT NULL UNIQUE,
+  `amount_owed` decimal(10,2) NOT NULL DEFAULT 0,
   `last_update` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `patient` VALUES 
+(1, 'John Smith', '1986-09-13', '(087) 7763497', 'jsmith@gmail.com', '150.00', '2019-12-18 13:10:00'),
+(2, 'Conor Mahoney', '1973-03-06', '(086) 2060791', 'cmahoney@outlook.com', '70.50', '2020-02-01 15:15:00'),
+(3, 'Michael McDonnell', '1980-03-18', '(085) 8127963', 'mmcdonnell@gmail.com', '0.00', '2015-04-10 13:00:00'),
+(4, 'Brendan Shannon', '1970-03-02', '(083) 2055117', 'bshannon@gmail.com', '50.00', '2019-03-02 13:00:00'),
+(5, 'Ellen Malone', '1991-05-12', '(087) 5872036', 'emalone@gmail.com', '0.00', '2018-07-18 13:00:00'),
+(6, 'Carmel Flanagan', '1987-10-22', '(087) 5904954', 'cflanagan@ymail.com', '0.00', '2019-09-27 13:00:00'),
+(7, 'Maureen Gallagher', '1983-04-05', '(086) 6041827', 'mgallagher@gmail.com', '130.00', '2020-01-21 13:00:00'),
+(8, 'Kieran McEvoy', '1995-02-07', '(083) 1608656', 'kmcevoy@outlook.com', '0.00', '2017-03-08 13:00:00'),
+(9, 'Jacinta Quinlan', '1973-06-23', '(085) 1843925', 'jquinlan@gmail.com', '300.00', '2019-10-22 13:00:00');
+
 
 -- --------------------------------------------------------
 
@@ -37,6 +50,15 @@ CREATE TABLE IF NOT EXISTS `appointment` (
   CONSTRAINT FOREIGN KEY(`patient_id`) REFERENCES `patient`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+INSERT INTO `appointment` VALUES
+(1, 9, '2020-03-12 15:00:00', 'online', '0'),
+(2, 7, '2020-03-13 11:00:00', 'post', '1'),
+(3, 5, '2020-03-14 09:40:00', 'phone', '0'),
+(4, 3, '2020-04-15 12:30:00', 'online', '0'),
+(5, 1, '2020-04-16 13:00:00', 'drop-in', '0');
+
+
 -- --------------------------------------------------------
 
 --
@@ -47,12 +69,19 @@ DROP TABLE IF EXISTS `bill`;
 CREATE TABLE IF NOT EXISTS `bill` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `date_issued` date NOT NULL,
-  `amount_due` float NOT NULL,
+  `amount_due` decimal(10,2) NOT NULL,
   `appointment_id` int(10) UNSIGNED NOT NULL,
   `type` set('Appointment', 'Late Cancellation Fee'),
   PRIMARY KEY(`id`),
   CONSTRAINT FOREIGN KEY(`appointment_id`) REFERENCES `appointment`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `bill` VALUES
+(1, '2020-03-12', 100, 1, 'Appointment'),
+(2, '2020-03-13', 200, 2, 'Late Cancellation Fee'),
+(3, '2020-03-14', 75, 3, 'Appointment');
+
 
 -- --------------------------------------------------------
 
@@ -69,6 +98,12 @@ CREATE TABLE IF NOT EXISTS `payment` (
   CONSTRAINT FOREIGN KEY(`bill_id`) REFERENCES `bill`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+INSERT INTO `payment` VALUES
+(1, 1, 'card'),
+(2, 2, 'cash');
+
+
 -- --------------------------------------------------------
 
 --
@@ -79,11 +114,16 @@ DROP TABLE IF EXISTS `specialist`;
 CREATE TABLE IF NOT EXISTS `specialist` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `phone_no` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL UNIQUE,
+  `phone_no` varchar(20) NOT NULL UNIQUE,
+  `email` varchar(50) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `specialist` VALUES
+(1, "ABC Dental", "123 Fake St., Oranmore, Co. Galway", '(091) 915 7266', 'abc@dental.ie');
+
 
 -- --------------------------------------------------------
 
--- phpmyadmin_dump.sql
+++ phpmyadmin_dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 22, 2020 at 07:43 PM
+-- Generation Time: Mar 22, 2020 at 09:33 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -37,7 +37,18 @@ CREATE TABLE IF NOT EXISTS `appointment` (
   `cancelled` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `patient_id` (`patient_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `patient_id`, `date`, `booking_method`, `cancelled`) VALUES
+(1, 9, '2020-03-12 15:00:00', 'online', 0),
+(2, 7, '2020-03-13 11:00:00', 'post', 1),
+(3, 5, '2020-03-14 09:40:00', 'phone', 0),
+(4, 3, '2020-04-15 12:30:00', 'online', 0),
+(5, 1, '2020-04-16 13:00:00', 'drop-in', 0);
 
 -- --------------------------------------------------------
 
@@ -49,12 +60,21 @@ DROP TABLE IF EXISTS `bill`;
 CREATE TABLE IF NOT EXISTS `bill` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `date_issued` date NOT NULL,
-  `amount_due` float NOT NULL,
+  `amount_due` decimal(10,2) NOT NULL,
   `appointment_id` int(10) UNSIGNED NOT NULL,
   `type` set('Appointment','Late Cancellation Fee') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `appointment_id` (`appointment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`id`, `date_issued`, `amount_due`, `appointment_id`, `type`) VALUES
+(1, '2020-03-12', '100.00', 1, 'Appointment'),
+(2, '2020-03-13', '200.00', 2, 'Late Cancellation Fee'),
+(3, '2020-03-14', '75.00', 3, 'Appointment');
 
 -- --------------------------------------------------------
 
@@ -69,10 +89,27 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `dob` date DEFAULT NULL,
   `phone_no` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `amount_owed` float NOT NULL DEFAULT '0',
+  `amount_owed` decimal(10,2) NOT NULL DEFAULT '0.00',
   `last_update` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone_no` (`phone_no`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`id`, `name`, `dob`, `phone_no`, `email`, `amount_owed`, `last_update`) VALUES
+(1, 'John Smith', '1986-09-13', '(087) 7763497', 'jsmith@gmail.com', '150.00', '2019-12-18 13:10:00'),
+(2, 'Conor Mahoney', '1973-03-06', '(086) 2060791', 'cmahoney@outlook.com', '70.50', '2020-02-01 15:15:00'),
+(3, 'Michael McDonnell', '1980-03-18', '(085) 8127963', 'mmcdonnell@gmail.com', '0.00', '2015-04-10 12:00:00'),
+(4, 'Brendan Shannon', '1970-03-02', '(083) 2055117', 'bshannon@gmail.com', '50.00', '2019-03-02 13:00:00'),
+(5, 'Ellen Malone', '1991-05-12', '(087) 5872036', 'emalone@gmail.com', '0.00', '2018-07-18 12:00:00'),
+(6, 'Carmel Flanagan', '1987-10-22', '(087) 5904954', 'cflanagan@ymail.com', '0.00', '2019-09-27 12:00:00'),
+(7, 'Maureen Gallagher', '1983-04-05', '(086) 6041827', 'mgallagher@gmail.com', '130.00', '2020-01-21 13:00:00'),
+(8, 'Kieran McEvoy', '1995-02-07', '(083) 1608656', 'kmcevoy@outlook.com', '0.00', '2017-03-08 13:00:00'),
+(9, 'Jacinta Quinlan', '1973-06-23', '(085) 1843925', 'jquinlan@gmail.com', '300.00', '2019-10-22 12:00:00');
 
 -- --------------------------------------------------------
 
@@ -87,7 +124,15 @@ CREATE TABLE IF NOT EXISTS `payment` (
   `payment_method` enum('cash','card','cheque') NOT NULL,
   PRIMARY KEY (`id`),
   KEY `bill_id` (`bill_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `bill_id`, `payment_method`) VALUES
+(1, 1, 'card'),
+(2, 2, 'cash');
 
 -- --------------------------------------------------------
 
@@ -102,8 +147,18 @@ CREATE TABLE IF NOT EXISTS `specialist` (
   `address` varchar(100) NOT NULL,
   `phone_no` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `address` (`address`),
+  UNIQUE KEY `phone_no` (`phone_no`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `specialist`
+--
+
+INSERT INTO `specialist` (`id`, `name`, `address`, `phone_no`, `email`) VALUES
+(1, 'ABC Dental', '123 Fake St., Oranmore, Co. Galway', '(091) 915 7266', 'abc@dental.ie');
 
 -- --------------------------------------------------------
 
