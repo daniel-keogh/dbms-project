b9419904f5e5192fa2af3576c11bbb46668cdef8
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-22 18:56:18 +0000

    Update mulcahy_dental.sql

--- mulcahy_dental.sql
+++ mulcahy_dental.sql
@@ -10,11 +10,11 @@ USE `mulcahy_dental`;
 
 DROP TABLE IF EXISTS `appointments`;
 CREATE TABLE IF NOT EXISTS `appointments` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `patient_id` int(10) NOT NULL,
   `date` datetime NOT NULL,
-  `booking` enum('online','post','phone','drop-in') NOT NULL,
-  `is_cancelled` tinyint(1) NOT NULL DEFAULT '0',
-  `follow_up` datetime NOT NULL,
+  `booking_method` enum('online','post','phone','drop-in') NOT NULL,
+  `cancelled` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -26,11 +26,11 @@ CREATE TABLE IF NOT EXISTS `appointments` (
 
 DROP TABLE IF EXISTS `bill`;
 CREATE TABLE IF NOT EXISTS `bill` (
-  `id` int(10) NOT NULL,
-  `date` date NOT NULL,
-  `amount` float NOT NULL,
-  `patient_id` int(10) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date_issued` date NOT NULL,
+  `amount_due` float NOT NULL,
   `appointment_id` int(10) NOT NULL,
+  `type` set('Appointment', 'Late Cancellation Fee'),
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,11 +42,13 @@ CREATE TABLE IF NOT EXISTS `bill` (
 
 DROP TABLE IF EXISTS `patient`;
 CREATE TABLE IF NOT EXISTS `patient` (
-  `id` int(10) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `dob` date DEFAULT NULL,
-  `last_appointment` date DEFAULT NULL,
-  `amt_owed` float NOT NULL,
+  `phone_no` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `amount_owed` float NOT NULL DEFAULT 0,
+  `last_update` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -58,10 +60,9 @@ CREATE TABLE IF NOT EXISTS `patient` (
 
 DROP TABLE IF EXISTS `payment`;
 CREATE TABLE IF NOT EXISTS `payment` (
-  `id` int(10) NOT NULL,
-  `amount` float NOT NULL,
-  `payment_type` enum('cash','card','cheque') NOT NULL,
-  `patient_id` int(10) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `bill_id` int(10) NOT NULL,
+  `payment_method` enum('cash','card','cheque') NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -73,10 +74,11 @@ CREATE TABLE IF NOT EXISTS `payment` (
 
 DROP TABLE IF EXISTS `specialist`;
 CREATE TABLE IF NOT EXISTS `specialist` (
-  `id` int(10) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `address` varchar(100) NOT NULL,
   `phone_no` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -88,9 +90,13 @@ CREATE TABLE IF NOT EXISTS `specialist` (
 
 DROP TABLE IF EXISTS `treatment`;
 CREATE TABLE IF NOT EXISTS `treatment` (
-  `id` int(10) NOT NULL,
-  `patient_id` int(10) NOT NULL,
-  `appointment_id` int(11) NOT NULL,
-  `image` blob NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `appointment_id` int(10) NOT NULL,
+  `appointment_card` longblob NOT NULL,
+  `appointment_card_path` varchar(50) NOT NULL,
+  `image` longblob DEFAULT NULL,
+  `image_path` varchar(50) DEFAULT NULL,
+  `follow_up` int(10) DEFAULT NULL,
+  `referral` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='treatment';
+) COLLATE utf8mb4_general_ci, ENGINE=InnoDB;
