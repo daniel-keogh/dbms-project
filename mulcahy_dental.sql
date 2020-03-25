ea36eb64b2c3029ecb746b9b291db9fd64955189
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-25 10:50:30 +0000

    Add more test data

--- mulcahy_dental.sql
+++ mulcahy_dental.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `appointment` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `patient_id` int(10) UNSIGNED NOT NULL,
   `date` datetime NOT NULL,
-  `booking_method` enum('online','post','phone','drop-in') NOT NULL,
+  `booking_method` enum('online','post','phone','drop-in','follow-up') NOT NULL,
   `cancelled` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   CONSTRAINT FOREIGN KEY(`patient_id`) REFERENCES `patient`(`id`)
@@ -60,18 +60,20 @@ INSERT INTO `appointment` VALUES
 (1, 15, '2020-03-12 15:00:00', 'online', '0'),
 (2, 14, '2020-03-13 11:00:00', 'post', '1'),
 (3, 13, '2020-03-14 09:40:00', 'phone', '0'),
-(4, 12, '2020-03-15 12:30:00', 'online', '0'),
+(4, 12, '2020-03-16 12:00:00', 'online', '0'),
 (5, 11, '2020-03-16 13:00:00', 'online', '1'),
 (6, 10, '2020-03-16 15:00:00', 'drop-in', '0'),
 (7, 9, '2020-03-17 13:00:00', 'drop-in', '0'),
-(8, 8, '2020-04-17 15:00:00', 'drop-in', '0'),
-(9, 7, '2020-04-17 17:00:00', 'drop-in', '0'),
-(10, 6, '2020-04-18 10:00:00', 'drop-in', '0'),
+(8, 8, '2020-03-17 15:00:00', 'drop-in', '0'),
+(9, 7, '2020-03-17 17:00:00', 'drop-in', '0'),
+(10, 6, '2020-03-18 10:00:00', 'drop-in', '0'),
 (11, 5, '2020-04-18 11:30:00', 'online', '0'),
 (12, 4, '2020-04-18 13:00:00', 'post', '0'),
 (13, 3, '2020-04-18 15:00:00', 'phone', '0'),
 (14, 2, '2020-04-18 17:00:00', 'phone', '0'),
 (15, 1, '2020-04-21 10:00:00', 'phone', '0'),
+(16, 1, '2020-05-04 10:00:00', 'follow-up', '0'),
+(17, 1, '2020-05-04 11:00:00', 'follow-up', '0'),
 
 
 -- --------------------------------------------------------
@@ -93,12 +95,16 @@ CREATE TABLE IF NOT EXISTS `bill` (
 
 
 INSERT INTO `bill` VALUES
-(1, '2020-03-12', 100, 1, 'Appointment'),
-(2, '2020-03-13', 200, 2, 'Late Cancellation Fee'),
-(3, '2020-03-14', 75, 3, 'Appointment'),
-(4, '2020-03-15', 100, 4, 'Appointment'),
-(5, '2020-03-16', 200, 5, 'Late Cancellation Fee'),
-(6, '2020-03-16', 75, 6, 'Appointment');
+(1, '2020-03-16', 100, 1, 'Appointment'),
+(2, '2020-03-16', 200, 2, 'Late Cancellation Fee'),
+(3, '2020-03-16', 75, 3, 'Appointment'),
+(4, '2020-03-16', 100, 4, 'Appointment'),
+(5, '2020-03-23', 200, 5, 'Late Cancellation Fee'),
+(6, '2020-03-23', 75, 6, 'Appointment'),
+(7, '2020-03-23', 100, 7, 'Appointment'),
+(8, '2020-03-23', 200, 8, 'Appointment'),
+(9, '2020-03-23', 75, 9, 'Appointment')
+(10, '2020-03-23', 100, 10, 'Appointment');
 
 
 -- --------------------------------------------------------
@@ -121,7 +127,13 @@ INSERT INTO `payment` VALUES
 (1, 1, 'card'),
 (2, 2, 'cash'),
 (3, 3, 'card'),
-(4, 4, 'cheque');
+(4, 4, 'cheque'),
+(5, 5, 'card'),
+(6, 6, 'cheque')
+(7, 7, 'card'),
+(8, 8, 'cheque')
+(9, 9, 'card'),
+(10, 10, 'cheque')
 
 
 -- --------------------------------------------------------
@@ -167,3 +179,16 @@ CREATE TABLE IF NOT EXISTS `treatment` (
   CONSTRAINT FOREIGN KEY(`follow_up`) REFERENCES `appointment`(`id`),
   CONSTRAINT FOREIGN KEY(`referral`) REFERENCES `specialist`(`id`)
 ) COLLATE utf8mb4_general_ci, ENGINE=InnoDB;
+
+
+INSERT INTO `treatment` VALUES
+(1, 1, load_file('c:/appointments/app_1/card.pdf'), '/app_1/card.pdf', load_file('c:/appointments/app_1/image.jpg'), '/app_1/image.jpg', NULL, NULL),
+(2, 2, load_file('c:/appointments/app_2/card.pdf'), '/app_2/card.pdf', load_file('c:/appointments/app_2/image.jpg'), '/app_2/image.jpg', NULL, 1);
+(3, 3, load_file('c:/appointments/app_3/card.pdf'), '/app_3/card.pdf', load_file('c:/appointments/app_3/image.jpg'), '/app_3/image.jpg', NULL, NULL),
+(4, 4, load_file('c:/appointments/app_4/card.pdf'), '/app_4/card.pdf', load_file('c:/appointments/app_4/image.jpg'), '/app_4/image.jpg', NULL, 2);
+(5, 5, load_file('c:/appointments/app_5/card.pdf'), '/app_5/card.pdf', NULL, NULL, NULL, NULL),
+(6, 6, load_file('c:/appointments/app_6/card.pdf'), '/app_6/card.pdf', NULL, NULL, NULL, 1);
+(7, 7, load_file('c:/appointments/app_7/card.pdf'), '/app_7/card.pdf', NULL, NULL, NULL, NULL),
+(8, 8, load_file('c:/appointments/app_8/card.pdf'), '/app_8/card.pdf', NULL, NULL, NULL, 2);
+(9, 9, load_file('c:/appointments/app_9/card.pdf'), '/app_9/card.pdf', load_file('c:/appointments/app_9/image.jpg'), '/app_9/image.jpg', 16, NULL),
+(10, 10, load_file('c:/appointments/app_10/card.pdf'), '/app_10/card.pdf', load_file('c:/appointments/app_10/image.jpg'), '/app_10/image.jpg', 17, NULL);
